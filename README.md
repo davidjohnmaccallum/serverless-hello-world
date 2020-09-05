f389d1f3add15dbe7f54fed8b5185811df534655
David MacCallum <davidjohnmac@gmail.com>
2020-09-05 23:18:45 +0200

    Added /hello-world route

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Just watched [Microservices & Serverless](https://www.youtube.com/watch?v=CKLIL3Kbf60). Trying out Serverless.
 
+This is the getting started program that Serverless creates.
 
+## Thoughts
+
+* I'm wondering how you run this program locally.
--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require('express');
 const app = express();
 
 // Routes
+app.get('/hello-world', (req, res) => {
+  const name = req.query.name ? req.query.name : 'World';
+  res.send(`Hello ${name}!`);
+});
+
 app.get('/*', (req, res) => {
   res.send(`Request received: ${req.method} - ${req.path}`);
 });
